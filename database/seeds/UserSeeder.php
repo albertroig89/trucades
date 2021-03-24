585e65e6c2131e48093f2ecc75632c1d09043f53
albertroig89 <albertroiglg@gmail.com>
2021-03-24 21:15:37 +0100

    5nt commit

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Department;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,11 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $techId = DB::table('departments')->where('title', 'Tecnic')->value('id');
+
+        $progId = Department::where('title', 'Programador')->value('id');
+        $admid = Department::where('title', 'Administracio')->value('id');
+        $comId = Department::where('title', 'Comercial')->value('id');
+
+
         User::create([
             'name' => 'Albert Roig',
             'email' => 'albert@microdelta.net',
             'password' => bcrypt('laravel'),
-            'Department_id' => 1,
+            'Department_id' => $techId,
             'is_admin' => true
         ]);
 
@@ -28,35 +36,35 @@ class UserSeeder extends Seeder
             'name' => 'Joel Valor',
             'email' => 'joel@microdelta.net',
             'password' => bcrypt('laravel2'),
-            'Department_id' => 1,
+            'Department_id' => $techId,
         ]); //EXERCICI TEMA 13 FET EN CONSTRUCTOR DE CONSULTES DE LARAVEL
 
         User::create([
             'name' => 'Josep Castells',
             'email' => 'josep@microdelta.net',
             'password' => bcrypt('laravel3'),
-            'Department_id' => 1,
+            'Department_id' => $techId,
         ]);
 
         User::create([
             'name' => 'Cristina Esquerre',
             'email' => 'administracio@microdelta.net',
             'password' => bcrypt('laravel4'),
-            'Department_id' => 2,
+            'Department_id' => $admid,
         ]);
 
         User::create([
             'name' => 'Manel Bel',
             'email' => 'comercial@microdelta.net',
             'password' => bcrypt('laravel5'),
-            'Department_id' => 4,
+            'Department_id' => $comId,
         ]);
 
         factory(User::class)->create([  //Crea usuari en nom aleatori i les dades que yo li paso
             'name' => 'Juan Valor',
             'email' => 'soft@microdelta.net',
             'password' => bcrypt('pass1234'),
-            'Department_id' => 3,
+            'Department_id' => $progId,
         ]);
 
     }
