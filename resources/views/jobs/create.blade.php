89967e4735049bb86a70cb0187665f15162051e6
albertroig89 <albertroiglg@gmail.com>
2021-05-13 21:35:24 +0200

    formulari feines casi acabat

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,59 +12,136 @@
                 {!! csrf_field() !!}
 
                 <div class="form-group">
+
+                    @if ($errors->has('client_id'))
+                        <div class="form-group is-invalid">
+                            <label for="selector-clients">Client:</label>
+                            <select class='form-control selector-clients' name='client_id' id='client_id'>
+                                <option value="">Selecciona un client</option>
+                                @foreach ($clients as $client)
+                                    <option value="{{ ($client->id) }}">{{ $client->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="invalid-feedback">
+                            {{ $errors->first('client_id') }}
+                        </div>
+                    @elseif ($errors->any())
+                        <div class="form- is-valid">
+                            <label for="selector-clients">Client:</label>
+                            <select class='form-control selector-clients' name='client_id' id='client_id'>
+                                <option value="">Selecciona un client</option>
+                                @foreach ($clients as $client)
+                                    <option value="{{ ($client->id) }}">{{ $client->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="valid-feedback">
+                            Correcte!
+                        </div>
+                    @else
+                        <div class="form-group">
+                            <label for="selector-clients">Client:</label>
+                            <select class='form-control selector-clients' name='client_id' id='client_id'>
+                                <option value="">Selecciona un client</option>
+                                @foreach ($clients as $client)
+                                    <option value="{{ ($client->id) }}">{{ $client->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    @endif
                     <div class="form-group">
-                        <label for="selector-clients">Client:</label>
-                        <select class='form-control selector-clients' name='client_id' id='client_id'>
-                            <option value="">Selecciona un client</option>
-                            @foreach ($clients as $client)
-                                <option value="{{ ($client->id) }}">{{ $client->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="datetimepicker_mask">Inici trucada:</label>
-                        <input type="text" class="form-control" value="" id="datetimepicker_mask"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="datetimepicker_mask2">Final trucada:</label>
-                        <input type="text" class="form-control" value="" id="datetimepicker_mask2"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="callinf">Informació de la feina:</label>
-                        @if ($errors->has('callinf'))
-                            <textarea name="callinf" class="form-control is-invalid" id="callinf">{{ old('callinf') }}</textarea>
+                        <label for="inittime">Inici trucada:</label>
+                        @if ($errors->has('inittime'))
+                            <input type="text" class="form-control is-invalid" value="{{ old('inittime') }}" id="inittime"/>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('inittime') }}
+                            </div>
                         @elseif ($errors->any())
-                            <textarea name="callinf" class="form-control is-valid" id="callinf">{{ old('callinf') }}</textarea>
+                            <input type="text" class="form-control is-valid" value="{{ old('inittime') }}" id="inittime"/>
                             <div class="valid-feedback">
                                 Correcte!
                             </div>
                         @else
-                            <textarea name="callinf" class="form-control" id="callinf">{{ old('callinf') }}</textarea>
+                            <input type="text" class="form-control" value="{{ old('inittime') }}" id="inittime"/>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="endtime">Final trucada:</label>
+                        @if ($errors->has('endtime'))
+                            <input type="text" class="form-control is-invalid" value="{{ old('endtime') }}" id="endtime"/>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('endtime') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <input type="text" class="form-control is-valid" value="{{ old('endtime') }}" id="endtime"/>
+                            <div class="valid-feedback">
+                                Correcte!
+                            </div>
+                        @else
+                            <input type="text" class="form-control" value="{{ old('endtime') }}" id="endtime"/>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label for="callinf">Informació de la feina:</label>
+                        @if ($errors->has('job'))
+                            <textarea name="job" class="form-control is-invalid" id="job">{{ old('job') }}</textarea>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('job') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <textarea name="job" class="form-control is-valid" id="job">{{ old('job') }}</textarea>
+                            <div class="valid-feedback">
+                                Correcte!
+                            </div>
+                        @else
+                            <textarea name="job" class="form-control" id="job">{{ old('job') }}</textarea>
                         @endif
                     </div>
                     <div class="form-group">
                         <label for="user_id">Empleat:</label>
-                        <select class="form-control" name="user_id" id="user_id">
-                            <option value="">Sel·lecciona empleat</option>
-                            @foreach ($users as $user)
-                                <option class="form-control" value="{{ ($user->id) }}">{{ $user->name }}</option>
-                            @endforeach
-                        </select>
-                        <div class="invalid-feedback">Example invalid custom select feedback</div>
+                        @if ($errors->has('user_id'))
+                            <select class="form-control is-invalid" name="user_id" id="user_id">
+                                <option value="">Sel·lecciona empleat</option>
+                                @foreach ($users as $user)
+                                    <option class="form-control is-invalid" value="{{ ($user->id) }}">{{ $user->name }}</option>
+                                @endforeach
+                            </select>
+                            <div class="invalid-feedback">
+                                {{ $errors->first('user_id') }}
+                            </div>
+                        @elseif ($errors->any())
+                            <select class="form-control is-valid" name="user_id" id="user_id">
+                                <option value="">Sel·lecciona empleat</option>
+                                @foreach ($users as $user)
+                                    <option class="form-control is-valid" value="{{ ($user->id) }}">{{ $user->name }}</option>
+                                @endforeach
+                            </select>
+                            <div class="valid-feedback">
+                                Correcte!
+                            </div>
+                        @else
+                            <select class="form-control" name="user_id" id="user_id">
+                                <option value="">Sel·lecciona empleat</option>
+                                @foreach ($users as $user)
+                                    <option class="form-control" value="{{ ($user->id) }}">{{ $user->name }}</option>
+                                @endforeach
+                            </select>
+                        @endif
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Afegir feina</button>
                 <a href="{{ route('home') }}" class="btn btn-default float-right">Tornar a l'inici</a>
             </form>
 
-            @if ($errors->any())
-                <br>
-                <div class="form-group">
-                    <div class="alert alert-danger">
-                        <h5>Por favor corrige los errores mencionados arriba {{ $errors }}</h5>
-                    </div>
-                </div>
-            @endif
+{{--            @if ($errors->any())--}}
+{{--                <br>--}}
+{{--                <div class="form-group">--}}
+{{--                    <div class="alert alert-danger">--}}
+{{--                        <h5>Por favor corrige los errores mencionados arriba {{ $errors }}</h5>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            @endif--}}
         </div>
     </div>
 
