6c6f688748631e87731da02badfb163d244da365
albertroig89 <albertroiglg@gmail.com>
2021-06-27 18:43:47 +0200

    deploy

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="es" class="h-100">
-  <head>ss
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -13,7 +13,6 @@
 
 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=" crossorigin="anonymous" />
-{{--      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">--}}
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.css"/>
       <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet"/>
 
@@ -23,9 +22,10 @@
       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
       <link rel="stylesheet" type="text/css" href="/css/styles.css" media="screen" />
 
-
       <!-- Favicons -->
-      <link  rel="icon"   href="../../images/favicon.png" type="image/png" />
+      <link  rel="icon"   href="../images/favicon.png" type="image/png" />
+
+
 
 
   </head>
@@ -140,10 +140,6 @@
   </div>
 </footer>
   </body>
-{{--  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>--}}
-{{--  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>--}}
-
-
 
   <script src="{{ asset('js/app.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
