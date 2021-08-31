2f3286de2de37430c9338d5392e35ecf033ebd9f
albertroig89 <albertroiglg@gmail.com>
2021-08-31 10:04:06 +0200

    contador hores totals

--- resources/views/jobs/count.blade.php
+++ resources/views/jobs/count.blade.php
@@ -17,6 +17,7 @@
                             <th scope="col">Temps total feines</th>
                             <th scope="col">Temps total historic</th>
                             <th scope="col">Temps total historic ocult</th>
+                            <th scope="col">Total temps</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -51,6 +52,25 @@
                                     @endforeach
                                     {{$contador}} min
                                 </td>
+                                <th>
+                                    <?php $contador=0 ?>
+                                    @foreach ($jobs as $job)
+                                        @if ($user->id == $job->user_id)
+                                            <?php $contador = $contador + $job->totalmin ?>
+                                        @endif
+                                    @endforeach
+                                    @foreach ($histjobs as $histjob)
+                                        @if ($user->name == $histjob->username)
+                                            <?php $contador = $contador + $histjob->totalmin ?>
+                                        @endif
+                                    @endforeach
+                                    @foreach ($histjobs2 as $histjob2)
+                                        @if ($user->name == $histjob2->username)
+                                            <?php $contador = $contador + $histjob2->totalmin ?>
+                                        @endif
+                                    @endforeach
+                                    {{$contador}} min
+                                </th>
                             </tr>
                         @endforeach
                         </tbody>
